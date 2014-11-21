4c05e24249304956bafa5c0d436773b11f81afcf
David Hendrix <nicktarix@googlemail.com>
2014-11-21 13:15:26 +0100

    Added BackPack Recipes

--- src/main/java/de/getsetsociety/gw2readr/v1/item/recipes/enums/RecipeType.java
+++ src/main/java/de/getsetsociety/gw2readr/v1/item/recipes/enums/RecipeType.java
@@ -1,64 +1,63 @@
 package de.getsetsociety.gw2readr.v1.item.recipes.enums;
 
 public enum RecipeType {
-	Component,
+    Component,
 
-	//Chef
-	Soup,
-	Meal,
-	Snack,
-	Dessert,
-	Feast,
-	IngredientCooking,
-	Seasoning,
-	
-	//Armor
-	Boots,
-	Helm,
-	Leggings,
-	Gloves,
-	Shoulders,
-	Coat,
-	
-	//Weapon
-	Axe,
-	Dagger,
-	Focus,
-	Greatsword,
-	Hammer,
-	Harpoon,
-	LongBow,
-	Mace,
-	Pistol,
-	Rifle,
-	Scepter,
-	Shield,
-	ShortBow,
-	Speargun,
-	Sword,
-	Staff,
-	Torch,
-	Trident,
-	Warhorn,
+    // Chef
+    Soup,
+    Meal,
+    Snack,
+    Dessert,
+    Feast,
+    IngredientCooking,
+    Seasoning,
 
-	
-	//Jeweler
-	Amulet,
-	Earring,
-	Ring,
-	
-	//Misc
-	Bag,
-	Bulk,
-	Consumable,
-	Dye,
-	Inscription,
-	Insignia,
-	Potion,
-	Refinement,
-	RefinementEctoplasm,
-	RefinementObsidian,
-	UpgradeComponent
-	
+    // Armor
+    Boots,
+    Helm,
+    Leggings,
+    Gloves,
+    Shoulders,
+    Coat,
+
+    // Weapon
+    Axe,
+    Dagger,
+    Focus,
+    Greatsword,
+    Hammer,
+    Harpoon,
+    LongBow,
+    Mace,
+    Pistol,
+    Rifle,
+    Scepter,
+    Shield,
+    ShortBow,
+    Speargun,
+    Sword,
+    Staff,
+    Torch,
+    Trident,
+    Warhorn,
+
+    // Jeweler
+    Amulet,
+    Earring,
+    Ring,
+
+    // Misc
+    Bag,
+    Bulk,
+    Consumable,
+    Dye,
+    Inscription,
+    Insignia,
+    Potion,
+    Refinement,
+    RefinementEctoplasm,
+    RefinementObsidian,
+    UpgradeComponent,
+    BackPack
 
 }
