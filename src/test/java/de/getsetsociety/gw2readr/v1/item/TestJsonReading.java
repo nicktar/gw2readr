da6fdc57f07e142d87256f1374dd5e03bf1457e0
David Hendrix <nicktarix@googlemail.com>
2014-11-21 15:02:20 +0100

    removed obsolete testcase

--- src/test/java/de/getsetsociety/gw2readr/v1/item/TestJsonReading.java
+++ src/test/java/de/getsetsociety/gw2readr/v1/item/TestJsonReading.java
@@ -1,4 +1,4 @@
-package  de.getsetsociety.gw2readr.v1.item;
+package de.getsetsociety.gw2readr.v1.item;
 
 import static org.junit.Assert.*;
 
@@ -6,10 +6,6 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.Map;
 import java.util.Map.Entry;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
 import org.junit.Test;
 
@@ -23,16 +19,12 @@ import de.getsetsociety.gw2readr.v0.item.items.enums.DamageType;
 import de.getsetsociety.gw2readr.v0.item.items.enums.ItemFlags;
 import de.getsetsociety.gw2readr.v0.item.items.enums.Rarity;
 import de.getsetsociety.gw2readr.v0.item.items.enums.WeaponType;
-import de.getsetsociety.gw2readr.v0.item.items.interfaces.IBaseItem;
 import de.getsetsociety.gw2readr.v1.colors.AllColorsReader;
 import de.getsetsociety.gw2readr.v1.colors.interfaces.IColor;
-import de.getsetsociety.gw2readr.v1.factories.EntityFactoryProvider;
-import de.getsetsociety.gw2readr.v1.factories.HibernateItemEntityFactory;
 import de.getsetsociety.gw2readr.v1.files.FilesReader;
 import de.getsetsociety.gw2readr.v1.files.interfaces.IFileInfo;
 import de.getsetsociety.gw2readr.v1.item.allitems.json.AllItems;
 import de.getsetsociety.gw2readr.v1.item.items.ItemReader;
-import de.getsetsociety.gw2readr.v1.item.items.hibernateentities.UpgradeComponent;
 import de.getsetsociety.gw2readr.v1.item.items.interfaces.IArmor;
 import de.getsetsociety.gw2readr.v1.item.items.interfaces.IBag;
 import de.getsetsociety.gw2readr.v1.item.items.interfaces.IConsumable;
@@ -43,142 +35,123 @@ import de.getsetsociety.gw2readr.v1.item.items.json.ItemJson;
 
 public class TestJsonReading {
 
-	private ItemReader reader = new ItemReader();
+    private ItemReader reader = new ItemReader();
 
-	@Test
-	public void testCraftingMaterialLemongrass() throws JsonParseException, JsonMappingException, MalformedURLException, IOException {
-		ICraftingMaterial cm = (ICraftingMaterial) reader.readItem(12546);
-		assertEquals(true, cm.getAvailableInActivity());
-		assertEquals(true, cm.getAvailableInDungeon());
-		assertEquals(true, cm.getAvailableInPvE());
-		assertEquals(false, cm.getAvailableInPvP());
-		assertEquals(false, cm.getAvailableInPvPLobby());
-		assertEquals(true, cm.getAvailableInWvW());
-		assertEquals("Ingredient", cm.getDescription());
-		assertEquals(true, cm.getFlags().isEmpty());
-		assertEquals(Integer.valueOf(219490), cm.getIconFileId());
-		assertEquals("DF5612F93A11DB5F53FECA523F50D15DBF5A9AA5", cm.getIconFileSignature());
-		assertEquals("Lemongrass", cm.getName());
-		assertEquals(Integer.valueOf(12546), cm.getId());
-		assertEquals(Integer.valueOf(80), cm.getLevel());
-		assertEquals(Rarity.Basic, cm.getRarity());
-		assertEquals(Integer.valueOf(9), cm.getVendorValue());
-	}
+    @Test
+    public void testCraftingMaterialLemongrass() throws JsonParseException, JsonMappingException, MalformedURLException, IOException {
+        ICraftingMaterial cm = (ICraftingMaterial) reader.readItem(12546);
+        assertEquals(true, cm.getAvailableInActivity());
+        assertEquals(true, cm.getAvailableInDungeon());
+        assertEquals(true, cm.getAvailableInPvE());
+        assertEquals(false, cm.getAvailableInPvP());
+        assertEquals(false, cm.getAvailableInPvPLobby());
+        assertEquals(true, cm.getAvailableInWvW());
+        assertEquals("Ingredient", cm.getDescription());
+        assertEquals(true, cm.getFlags().isEmpty());
+        assertEquals(Integer.valueOf(219490), cm.getIconFileId());
+        assertEquals("DF5612F93A11DB5F53FECA523F50D15DBF5A9AA5", cm.getIconFileSignature());
+        assertEquals("Lemongrass", cm.getName());
+        assertEquals(Integer.valueOf(12546), cm.getId());
+        assertEquals(Integer.valueOf(80), cm.getLevel());
+        assertEquals(Rarity.Basic, cm.getRarity());
+        assertEquals(Integer.valueOf(9), cm.getVendorValue());
+    }
 
-	@Test
-	public void testWeaponWayfarersTribalBow() throws MalformedURLException, IOException {
-		IWeapon o = (IWeapon)reader.readItem(38875);
-		assertEquals(DamageType.Physical, o.getDamageType());
-		assertEquals(Integer.valueOf(0), o.getDefense());
-		IInfixUpgrade upgrade = o.getInfixUpgrade();
-		assertEquals(null, upgrade.getBuff());
-		assertTrue(upgrade.getAttributes().isEmpty());
-		assertTrue(o.getInfusionSlots().isEmpty());
-		assertEquals(Integer.valueOf(1080), o.getMaxPower());
-		assertEquals(Integer.valueOf(920), o.getMinPower());
-		assertEquals(null, o.getSuffixItemId());
-		assertEquals(WeaponType.LongBow, o.getWeaponType());
-		assertEquals(Integer.valueOf(38875), o.getId());
-		assertEquals("Wayfarer's Tribal Bow", o.getName());
-		assertEquals(Integer.valueOf(80), o.getLevel());
-		assertEquals(Rarity.Exotic, o.getRarity());
-		assertEquals(Integer.valueOf(396), o.getVendorValue());
-		assertEquals(Integer.valueOf(67297), o.getIconFileId());
-		assertEquals("A51ADB360705467301EC46B62D39BECFDEDE4810", o.getIconFileSignature());
-		assertTrue(o.getAvailableInActivity());
-		assertTrue(o.getAvailableInDungeon());
-		assertTrue(o.getAvailableInPvE());
-		assertFalse(o.getAvailableInPvP());
-		assertFalse(o.getAvailableInPvPLobby());
-		assertTrue(o.getAvailableInWvW());
-		assertTrue(o.getFlags().contains(ItemFlags.SoulBindOnUse));
-		assertTrue(o.getFlags().contains(ItemFlags.AccountBound));
-		assertEquals(2, o.getFlags().size());
-		assertEquals("", o.getDescription());
-	}
+    @Test
+    public void testWeaponWayfarersTribalBow() throws MalformedURLException, IOException {
+        IWeapon o = (IWeapon) reader.readItem(38875);
+        assertEquals(DamageType.Physical, o.getDamageType());
+        assertEquals(Integer.valueOf(0), o.getDefense());
+        IInfixUpgrade upgrade = o.getInfixUpgrade();
+        assertEquals(null, upgrade.getBuff());
+        assertTrue(upgrade.getAttributes().isEmpty());
+        assertTrue(o.getInfusionSlots().isEmpty());
+        assertEquals(Integer.valueOf(1080), o.getMaxPower());
+        assertEquals(Integer.valueOf(920), o.getMinPower());
+        assertEquals(null, o.getSuffixItemId());
+        assertEquals(WeaponType.LongBow, o.getWeaponType());
+        assertEquals(Integer.valueOf(38875), o.getId());
+        assertEquals("Tribal Bow", o.getName());
+        assertEquals(Integer.valueOf(80), o.getLevel());
+        assertEquals(Rarity.Exotic, o.getRarity());
+        assertEquals(Integer.valueOf(396), o.getVendorValue());
+        assertEquals(Integer.valueOf(67297), o.getIconFileId());
+        assertEquals("A51ADB360705467301EC46B62D39BECFDEDE4810", o.getIconFileSignature());
+        assertTrue(o.getAvailableInActivity());
+        assertTrue(o.getAvailableInDungeon());
+        assertTrue(o.getAvailableInPvE());
+        assertFalse(o.getAvailableInPvP());
+        assertFalse(o.getAvailableInPvPLobby());
+        assertTrue(o.getAvailableInWvW());
+        assertTrue(o.getFlags().contains(ItemFlags.SoulBindOnUse));
+        assertTrue(o.getFlags().contains(ItemFlags.AccountBound));
+        assertEquals(2, o.getFlags().size());
+        assertEquals(null, o.getDescription());
+    }
 
-	@Test
-	public void testConsumableLimitedUseBronzeDolyak() throws MalformedURLException, IOException {
-		IConsumable o = (IConsumable)reader.readItem(49464);
-		assertNotNull(o);
+    @Test
+    public void testConsumableLimitedUseBronzeDolyak() throws MalformedURLException, IOException {
+        IConsumable o = (IConsumable) reader.readItem(49464);
+        assertNotNull(o);
 
+    }
 
-	}
+    @Test
+    public void testArmorCarrionNobleBoots() throws MalformedURLException, IOException {
+        IArmor o = (IArmor) reader.readItem(12079);
+        assertNotNull(o);
 
-	@Test
-	public void testArmorCarrionNobleBoots() throws MalformedURLException, IOException {
-		IArmor o = (IArmor)reader.readItem(12079);
-		assertNotNull(o);
+    }
 
-	}
+    @Test
+    public void testBag18SlotInvisibleLeatherPack() throws MalformedURLException, IOException {
+        IBag o = (IBag) reader.readItem(9579);
+        assertNotNull(o);
 
-	@Test
-	public void testBag18SlotInvisibleLeatherPack() throws MalformedURLException, IOException {
-		IBag o = (IBag)reader.readItem(9579);
-		assertNotNull(o);
+    }
 
-	}
+    public void testFindNextUnknownType() throws MalformedURLException, IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        String content = "";
+        boolean start = true;
+        Integer count = 0;
+        try {
+            for (Integer i : mapper.readValue(ContentLoader.getV1ItemsUrlContent(), AllItems.class).getItems()) {
+                start = start || i.equals(49386);
+                if (start) {
+                    content = ContentLoader.getV1ItemUrlContent(String.valueOf(i), Language.English);
 
-	public void testFindNextUnknownType() throws MalformedURLException, IOException {
-		ObjectMapper mapper = new ObjectMapper();
-		String content = "";
-		boolean start = true;
-		Integer count = 0;
-		try {
-			for (Integer i: mapper.readValue(ContentLoader.getV1ItemsUrlContent(), AllItems.class).getItems()) {
-				start = start || i.equals(49386);
-				if(start) {
-					content = ContentLoader.getV1ItemUrlContent(String.valueOf(i), Language.English);
+                    ItemJson<?> item = mapper.readValue(content, ItemJson.class);
+                    assertTrue(displayMap(item.getAdditionalProperties()), item.getAdditionalProperties().isEmpty());
+                }
+                count++;
+            }
+        } finally {
+            System.out.println(count + " items gelesen");
+            System.out.println(content);
+        }
+    }
 
-					ItemJson<?> item = mapper.readValue(content, ItemJson.class);
-					assertTrue(displayMap(item.getAdditionalProperties()), item.getAdditionalProperties().isEmpty());
-				}
-				count++;
-			}
-		} finally {
-			System.out.println(count + " items gelesen");
-			System.out.println(content);
-		}
-	}
+    private String displayMap(Map<String, Object> map) {
+        StringBuilder retValue = new StringBuilder();
+        for (Entry<String, Object> e : map.entrySet()) {
+            retValue.append("Map entry: ").append(e.getKey()).append(" - ").append(e.getValue()).append("\n");
+        }
+        return retValue.toString();
+    }
 
-	private String displayMap(Map<String, Object> map) {
-		StringBuilder retValue = new StringBuilder();
-		for (Entry<String, Object> e: map.entrySet()) {
-			retValue.append("Map entry: ").append(e.getKey()).append(" - ").append(e.getValue()).append("\n");
-		}
-		return retValue.toString();
-	}
+    @Test
+    public void testReadingColors() {
+        AllColorsReader ar = new AllColorsReader();
+        Map<Integer, IColor> allColors = ar.readAllColors();
+        assertNotNull(allColors);
+    }
 
-	@Test
-	public void testReadintItem24586() {
-		ItemReader ir = new ItemReader();
-		EntityFactoryProvider.setItemEntityFactory(new HibernateItemEntityFactory());
-		IBaseItem i = ir.readItem(24589);
-		assertTrue(i.getDescription().length() < 512);
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("de.getsetsociety.gw2readr");
-
-		EntityManager em = emf.createEntityManager();
-
-		if (em.find(UpgradeComponent.class, 24589) != null) {
-			em.merge((UpgradeComponent)i);
-		} else {
-			em.persist(i);
-		}
-
-	}
-
-	@Test
-	public void testReadingColors() {
-		AllColorsReader ar = new AllColorsReader();
-		Map<Integer, IColor> allColors = ar.readAllColors();
-		assertNotNull(allColors);
-	}
-
-	@Test
-	public void testReadingFileInfos() {
-		FilesReader fir = new FilesReader();
-		Map<String, IFileInfo> allFiles = fir.readFiles();
-		assertNotNull(allFiles);
-		assertFalse(allFiles.isEmpty());
-	}
+    @Test
+    public void testReadingFileInfos() {
+        FilesReader fir = new FilesReader();
+        Map<String, IFileInfo> allFiles = fir.readFiles();
+        assertNotNull(allFiles);
+        assertFalse(allFiles.isEmpty());
+    }
 }
