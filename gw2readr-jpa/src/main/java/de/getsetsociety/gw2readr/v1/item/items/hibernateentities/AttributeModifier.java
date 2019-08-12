3e40dcabe3cfbcfb8c7f4028006dcf73acbeb78c
David Hendrix <nicktar@unbewaff.net>
2019-08-12 22:21:52 +0200

    lombokify

--- gw2readr-jpa/src/main/java/de/getsetsociety/gw2readr/v1/item/items/hibernateentities/AttributeModifier.java
+++ gw2readr-jpa/src/main/java/de/getsetsociety/gw2readr/v1/item/items/hibernateentities/AttributeModifier.java
@@ -1,55 +1,26 @@
 package de.getsetsociety.gw2readr.v1.item.items.hibernateentities;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
 import de.getsetsociety.gw2readr.v0.item.items.enums.Attribute;
 import de.getsetsociety.gw2readr.v1.item.items.interfaces.IAttributeModifier;
+import lombok.Data;
 
+import javax.persistence.*;
+
+@Data
 @Table
 @Entity
 public class AttributeModifier implements IAttributeModifier {
 
-	private Integer id;
-	private Attribute attribute;
-	private Integer modifier;
+    @Id
+    @Column
+    @GeneratedValue
+    private Integer id;
 
-	@Id
-	@Column
-	@GeneratedValue
-	public Integer getId() {
-		return id;
-	}
+    @Column
+    @Enumerated(EnumType.ORDINAL)
+    private Attribute attribute;
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    @Column
+    private Integer modifier;
 
-	@Override
-	@Column
-	@Enumerated(EnumType.ORDINAL)
-	public Attribute getAttribute() {
-		return this.attribute;
-	}
-
-	@Override
-	public void setAttribute(Attribute attribute) {
-		this.attribute = attribute;
-	}
-
-	@Override
-	@Column
-	public Integer getModifier() {
-		return this.modifier;
-	}
-
-	@Override
-	public void setModifier(Integer modifier) {
-		this.modifier = modifier;
-	}
 }
