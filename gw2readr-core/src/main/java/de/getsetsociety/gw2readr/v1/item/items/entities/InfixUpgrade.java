8f4254e0ead2c20a4eaf188167cec19dd8542fa2
David Hendrix <nicktar@unbewaff.net>
2019-08-12 22:13:16 +0200

    lombokify

--- gw2readr-core/src/main/java/de/getsetsociety/gw2readr/v1/item/items/entities/InfixUpgrade.java
+++ gw2readr-core/src/main/java/de/getsetsociety/gw2readr/v1/item/items/entities/InfixUpgrade.java
@@ -1,45 +1,18 @@
 
 package de.getsetsociety.gw2readr.v1.item.items.entities;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import de.getsetsociety.gw2readr.v1.item.items.interfaces.IAttributeModifier;
 import de.getsetsociety.gw2readr.v1.item.items.interfaces.IBuff;
 import de.getsetsociety.gw2readr.v1.item.items.interfaces.IInfixUpgrade;
+import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
 public class InfixUpgrade implements IInfixUpgrade {
 
+	private Integer id;
 	private List<IAttributeModifier> attributes = new ArrayList<>();
 	private IBuff buff;
-
-	/* (non-Javadoc)
-	 * @see de.getsetsociety.gw2readr.entities.IInfixUpgrade#getAttributes()
-	 */
-	@Override
-	public List<IAttributeModifier> getAttributes(){
-		return this.attributes;
-	}
-	/* (non-Javadoc)
-	 * @see de.getsetsociety.gw2readr.entities.IInfixUpgrade#setAttributes(java.util.List)
-	 */
-	@Override
-	public void setAttributes(List<IAttributeModifier> attributes){
-		this.attributes = attributes;
-	}
-
-	/* (non-Javadoc)
-	 * @see de.getsetsociety.gw2readr.entities.IInfixUpgrade#getBuff()
-	 */
-	@Override
-	public IBuff getBuff() {
-		return buff;
-	}
-	/* (non-Javadoc)
-	 * @see de.getsetsociety.gw2readr.entities.IInfixUpgrade#setBuff(de.getsetsociety.armory.items.IBuff)
-	 */
-	@Override
-	public void setBuff(IBuff buff) {
-		this.buff = buff;
-	}
 }
