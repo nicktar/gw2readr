e86b79b4786109517844246a0668cdf96a8a1906
David Hendrix <nicktar@unbewaff.net>
2019-08-12 22:03:06 +0200

    drop useless empty JavaDocs / add lombok / drop serializability

--- gw2readr-core/src/main/java/de/getsetsociety/gw2readr/v1/item/items/entities/Item.java
+++ gw2readr-core/src/main/java/de/getsetsociety/gw2readr/v1/item/items/entities/Item.java
@@ -1,25 +1,19 @@
-/**
- * 
- */
 package de.getsetsociety.gw2readr.v1.item.items.entities;
-
-import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Set;
 
 import de.getsetsociety.gw2readr.general.enums.Language;
 import de.getsetsociety.gw2readr.v0.item.items.enums.ItemFlags;
 import de.getsetsociety.gw2readr.v0.item.items.enums.Rarity;
 import de.getsetsociety.gw2readr.v0.item.items.enums.RestrictionType;
 import de.getsetsociety.gw2readr.v1.item.items.interfaces.IItem;
+import lombok.Data;
 
-/**
- * @author Nicktar
- * 
- */
-public class Item implements Serializable, IItem {
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
-	private static final long serialVersionUID = -8942935346941562118L;
+@Data
+public class Item implements IItem {
+
 	private Integer id;
 	private String name;
 	private Integer level;
@@ -37,246 +31,7 @@ public class Item implements Serializable, IItem {
 	private String description;
 	private Set<RestrictionType> restrictions = new HashSet<>();
 	private Language language;
-
-	/* (non-Javadoc)
-	 * @see de.getsetsociety.gw2readr.entities.IBaseItem#getId()
-	 */
-	@Override
-	public Integer getId() {
-		return id;
-	}
-
-	/* (non-Javadoc)
-	 * @see de.getsetsociety.gw2readr.entities.IBaseItem#setId(java.lang.Integer)
-	 */
-	@Override
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	/* (non-Javadoc)
-	 * @see de.getsetsociety.gw2readr.entities.IBaseItem#getName()
-	 */
-	@Override
-	public String getName() {
-		return name;
-	}
-
-	/* (non-Javadoc)
-	 * @see de.getsetsociety.gw2readr.entities.IBaseItem#setName(java.lang.String)
-	 */
-	@Override
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	/* (non-Javadoc)
-	 * @see de.getsetsociety.gw2readr.entities.IBaseItem#getLevel()
-	 */
-	@Override
-	public Integer getLevel() {
-		return level;
-	}
-
-	/* (non-Javadoc)
-	 * @see de.getsetsociety.gw2readr.entities.IBaseItem#setLevel(java.lang.Integer)
-	 */
-	@Override
-	public void setLevel(Integer level) {
-		this.level = level;
-	}
-
-	/* (non-Javadoc)
-	 * @see de.getsetsociety.gw2readr.entities.IBaseItem#getRarity()
-	 */
-	@Override
-	public Rarity getRarity() {
-		return rarity;
-	}
-
-	/* (non-Javadoc)
-	 * @see de.getsetsociety.gw2readr.entities.IBaseItem#setRarity(de.getsetsociety.armory.enums.Rarity)
-	 */
-	@Override
-	public void setRarity(Rarity rarity) {
-		this.rarity = rarity;
-	}
-
-	/* (non-Javadoc)
-	 * @see de.getsetsociety.gw2readr.entities.IBaseItem#getVendorValue()
-	 */
-	@Override
-	public Integer getVendorValue() {
-		return vendorValue;
-	}
-
-	/* (non-Javadoc)
-	 * @see de.getsetsociety.gw2readr.entities.IBaseItem#setVendorValue(java.lang.Integer)
-	 */
-	@Override
-	public void setVendorValue(Integer vendorValue) {
-		this.vendorValue = vendorValue;
-	}
-
-	/* (non-Javadoc)
-	 * @see de.getsetsociety.gw2readr.entities.IBaseItem#getIconFileId()
-	 */
-	@Override
-	public Integer getIconFileId() {
-		return iconFileId;
-	}
-
-	/* (non-Javadoc)
-	 * @see de.getsetsociety.gw2readr.entities.IBaseItem#setIconFileId(java.lang.Integer)
-	 */
-	@Override
-	public void setIconFileId(Integer iconFileId) {
-		this.iconFileId = iconFileId;
-	}
-
-	/* (non-Javadoc)
-	 * @see de.getsetsociety.gw2readr.entities.IBaseItem#getIconFileSignature()
-	 */
-	@Override
-	public String getIconFileSignature() {
-		return iconFileSignature;
-	}
-
-	/* (non-Javadoc)
-	 * @see de.getsetsociety.gw2readr.entities.IBaseItem#setIconFileSignature(java.lang.String)
-	 */
-	@Override
-	public void setIconFileSignature(String iconFileSignature) {
-		this.iconFileSignature = iconFileSignature;
-	}
-
-	/* (non-Javadoc)
-	 * @see de.getsetsociety.gw2readr.entities.IBaseItem#getAvailableInActivity()
-	 */
-	@Override
-	public Boolean getAvailableInActivity() {
-		return availableInActivity;
-	}
-
-	/* (non-Javadoc)
-	 * @see de.getsetsociety.gw2readr.entities.IBaseItem#setAvailableInActivity(java.lang.Boolean)
-	 */
-	@Override
-	public void setAvailableInActivity(Boolean availableInActivity) {
-		this.availableInActivity = availableInActivity;
-	}
-
-	/* (non-Javadoc)
-	 * @see de.getsetsociety.gw2readr.entities.IBaseItem#getAvailableInDungeon()
-	 */
-	@Override
-	public Boolean getAvailableInDungeon() {
-		return availableInDungeon;
-	}
-
-	/* (non-Javadoc)
-	 * @see de.getsetsociety.gw2readr.entities.IBaseItem#setAvailableInDungeon(java.lang.Boolean)
-	 */
-	@Override
-	public void setAvailableInDungeon(Boolean availableInDungeon) {
-		this.availableInDungeon = availableInDungeon;
-	}
-
-	/* (non-Javadoc)
-	 * @see de.getsetsociety.gw2readr.entities.IBaseItem#getAvailableInPvE()
-	 */
-	@Override
-	public Boolean getAvailableInPvE() {
-		return availableInPvE;
-	}
-
-	/* (non-Javadoc)
-	 * @see de.getsetsociety.gw2readr.entities.IBaseItem#setAvailableInPvE(java.lang.Boolean)
-	 */
-	@Override
-	public void setAvailableInPvE(Boolean availableInPvE) {
-		this.availableInPvE = availableInPvE;
-	}
-
-	/* (non-Javadoc)
-	 * @see de.getsetsociety.gw2readr.entities.IBaseItem#getAvailableInPvP()
-	 */
-	@Override
-	public Boolean getAvailableInPvP() {
-		return availableInPvP;
-	}
-
-	/* (non-Javadoc)
-	 * @see de.getsetsociety.gw2readr.entities.IBaseItem#setAvailableInPvP(java.lang.Boolean)
-	 */
-	@Override
-	public void setAvailableInPvP(Boolean availableInPvP) {
-		this.availableInPvP = availableInPvP;
-	}
-
-	/* (non-Javadoc)
-	 * @see de.getsetsociety.gw2readr.entities.IBaseItem#getAvailableInPvPLobby()
-	 */
-	@Override
-	public Boolean getAvailableInPvPLobby() {
-		return availableInPvPLobby;
-	}
-
-	/* (non-Javadoc)
-	 * @see de.getsetsociety.gw2readr.entities.IBaseItem#setAvailableInPvPLobby(java.lang.Boolean)
-	 */
-	@Override
-	public void setAvailableInPvPLobby(Boolean availableInPvPLobby) {
-		this.availableInPvPLobby = availableInPvPLobby;
-	}
-
-	/* (non-Javadoc)
-	 * @see de.getsetsociety.gw2readr.entities.IBaseItem#getAvailableInWvW()
-	 */
-	@Override
-	public Boolean getAvailableInWvW() {
-		return availableInWvW;
-	}
-
-	/* (non-Javadoc)
-	 * @see de.getsetsociety.gw2readr.entities.IBaseItem#setAvailableInWvW(java.lang.Boolean)
-	 */
-	@Override
-	public void setAvailableInWvW(Boolean availableInWvW) {
-		this.availableInWvW = availableInWvW;
-	}
-
-	/* (non-Javadoc)
-	 * @see de.getsetsociety.gw2readr.entities.IBaseItem#getFlags()
-	 */
-	@Override
-	public Set<ItemFlags> getFlags() {
-		return flags;
-	}
-
-	/* (non-Javadoc)
-	 * @see de.getsetsociety.gw2readr.entities.IBaseItem#setFlags(java.util.Set)
-	 */
-	@Override
-	public void setFlags(Set<ItemFlags> flags) {
-		this.flags = flags;
-	}
-
-	/* (non-Javadoc)
-	 * @see de.getsetsociety.gw2readr.entities.IBaseItem#getDescription()
-	 */
-	@Override
-	public String getDescription() {
-		return description;
-	}
-
-	/* (non-Javadoc)
-	 * @see de.getsetsociety.gw2readr.entities.IBaseItem#setDescription(java.lang.String)
-	 */
-	@Override
-	public void setDescription(String description) {
-		this.description = description;
-	}
+	private List<String> upgradeRecipes;
 
 	/* (non-Javadoc)
 	 * @see de.getsetsociety.gw2readr.entities.IBaseItem#string2GameType(java.lang.String)
@@ -284,52 +39,27 @@ public class Item implements Serializable, IItem {
 	@Override
 	public void string2GameType(String gameType) {
 		switch (gameType) {
-		case "Activity":
-			setAvailableInActivity(true);
-			break;
-		case "Dungeon":
-			setAvailableInDungeon(true);
-			break;
-		case "Pve":
-			setAvailableInPvE(true);
-			break;
-		case "Pvp":
-			setAvailableInPvP(true);
-			break;
-		case "PvpLobby":
-			setAvailableInPvPLobby(true);
-			break;
-		case "Wvw":
-			setAvailableInWvW(true);
-			break;
-		default:
-			System.out.println("Unknown game type: " + gameType);
+			case "Activity":
+				setAvailableInActivity(true);
+				break;
+			case "Dungeon":
+				setAvailableInDungeon(true);
+				break;
+			case "Pve":
+				setAvailableInPvE(true);
+				break;
+			case "Pvp":
+				setAvailableInPvP(true);
+				break;
+			case "PvpLobby":
+				setAvailableInPvPLobby(true);
+				break;
+			case "Wvw":
+				setAvailableInWvW(true);
+				break;
+			default:
+				System.out.println("Unknown game type: " + gameType);
 		}
 	}
 
-	/* (non-Javadoc)
-	 * @see de.getsetsociety.gw2readr.entities.IBaseItem#getRestrictions()
-	 */
-	@Override
-	public Set<RestrictionType> getRestrictions() {
-		return restrictions;
-	}
-
-	/* (non-Javadoc)
-	 * @see de.getsetsociety.gw2readr.entities.IBaseItem#setRestrictions(java.util.Set)
-	 */
-	@Override
-	public void setRestrictions(Set<RestrictionType> restrictions) {
-		this.restrictions = restrictions;
-	}
-
-	@Override
-	public Language getLanguage() {
-		return language;
-	}
-
-	@Override
-	public void setLanguage(Language language) {
-		this.language = language;
-	}
 }
