f063ac9c4bce1475b3f1872fe00778d1e11625da
David Hendrix <nicktar@unbewaff.net>
2019-08-12 22:04:35 +0200

    clean up code

--- gw2readr-core/src/main/java/de/getsetsociety/gw2readr/v1/item/items/interfaces/IItem.java
+++ gw2readr-core/src/main/java/de/getsetsociety/gw2readr/v1/item/items/interfaces/IItem.java
@@ -1,32 +1,32 @@
 package de.getsetsociety.gw2readr.v1.item.items.interfaces;
 
-import java.util.Set;
-
 import de.getsetsociety.gw2readr.v0.item.items.enums.ItemFlags;
 import de.getsetsociety.gw2readr.v0.item.items.enums.Rarity;
 import de.getsetsociety.gw2readr.v0.item.items.enums.RestrictionType;
 import de.getsetsociety.gw2readr.v0.item.items.interfaces.IBaseItem;
 
+import java.util.Set;
+
 public interface IItem extends IBaseItem {
 
-	public abstract Rarity getRarity();
+    Rarity getRarity();
 
-	public abstract void setRarity(Rarity rarity);
+    void setRarity(Rarity rarity);
 
-	public abstract Integer getIconFileId();
+    Integer getIconFileId();
 
-	public abstract void setIconFileId(Integer iconFileId);
+    void setIconFileId(Integer iconFileId);
 
-	public abstract String getIconFileSignature();
+    String getIconFileSignature();
 
-	public abstract void setIconFileSignature(String iconFileSignature);
+    void setIconFileSignature(String iconFileSignature);
 
-	public abstract Set<RestrictionType> getRestrictions();
+    Set<RestrictionType> getRestrictions();
 
-	public abstract void setRestrictions(Set<RestrictionType> restrictions);
+    void setRestrictions(Set<RestrictionType> restrictions);
 
-	public abstract Set<ItemFlags> getFlags();
+    Set<ItemFlags> getFlags();
 
-	public abstract void setFlags(Set<ItemFlags> flags);
+    void setFlags(Set<ItemFlags> flags);
 
 }
